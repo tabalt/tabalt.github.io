659f7da5523cbedf2af2a53b5b072708557deab3
tabalt <tabalt@actphp.com>
2014-05-26 11:25:01 +0800

    add out link nofollow

--- static/js/blog.js
+++ static/js/blog.js
@@ -4,6 +4,7 @@ window.onload = function () {
     for (var i = 0, linksLength = links.length; i < linksLength; i++) {
        if (links[i].hostname != window.location.hostname) {
            links[i].target = '_blank';
+           links[i].rel = 'nofollow';
        } 
     }
 }
